878d50c7b4493dd17e243632eb7dea5612aab340
Abhishek Sinha <avishkarabhishek786@gmail.com>
2022-07-02 18:32:17 +0530

    added Stocks, StocksExchange, StocksFactory.sol

--- contracts/StockExchange.sol
+++ contracts/StockExchange.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 
+interface IStocks is IERC20 {}
+interface IStocksFactory {
+    function getStocksData() external view returns (IStocks[] memory, bool[] memory);
+    function isStockTrading(uint id) external view returns (bool);
+}
+
 contract StockExchange {
 
-    IERC20 public USDC;
+    IERC20 public immutable USDC;
 
     constructor(address _usdc) {
         USDC = IERC20(_usdc);
--- contracts/StocksFactory.sol
+++ contracts/StocksFactory.sol
@@ -1,27 +1,47 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Stocks.sol";
  
-contract StocksFactory {
+contract StocksFactory is Ownable {
 
-    uint stocksCount;
-    mapping(uint=>Stocks) public stocksList;
+    uint public stocksCount;
 
-    function createStocks(string memory _name, string memory _symbol) external payable {
-        // todo: check for already existing token symbol as well
-        require(address(stocksList[stocksCount])==address(0), "Stock already exists.");
+    struct StockData {
+        Stocks stocks;
+        bool allowTrade;
+    }
+    mapping(uint=>StockData) public stocksList;
+
+    function createStocks(string memory _name, string memory _symbol) external payable onlyOwner {
+        // check for already existing token symbol as well
+        require(address(stocksList[stocksCount].stocks)==address(0), "Stock already exists.");
         Stocks newToken = new Stocks(_name, _symbol);
-        stocksList[stocksCount] = newToken;
+        stocksList[stocksCount] = StockData(newToken, true);
         stocksCount++;
     }
 
-    function getStocksList() external view returns (Stocks[] memory) {
+    function getStocksData() external view returns (Stocks[] memory, bool[] memory) {
         Stocks[] memory sl = new Stocks[](stocksCount);
+        bool[] memory isTradableLis = new bool[](stocksCount);
+
         for(uint i=0; i<stocksCount; i++) {
-            sl[i] = stocksList[i];
+            sl[i] = stocksList[i].stocks;
+            isTradableLis[i] = stocksList[i].allowTrade;
         }
-        return sl;
+        return (sl, isTradableLis);
     }
+
+    // Check if Stock exists and trading
+    function isStockTrading(uint id) public view returns (bool) {
+        return stocksList[id].allowTrade;
+    }
+
+    // Create function to disable a stock from trading
+    function disableStock(uint id) external onlyOwner {
+        stocksList[id].allowTrade = false;
+    }
+    
 
 }
